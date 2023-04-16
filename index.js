b70c6526442b39dec542c537c2434bfa8e769e14
BenjaminJacobson <ben.jacobson@flatironschool.com>
2023-04-16 15:58:02 -0400

    completed tests

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(arr,eventName) {
+    let newArr = []
+    for (let i = 0; i < arr.length; i++) {
+        let newMessage = `Thank you, ${arr[i]}, for the wonderful ${eventName} gift!`
+        newArr.push(newMessage)
+    }
+    return newArr
+}
+
+function countDown(num) {
+    let count = num
+    while (count >= 0) {
+    console.log(count)
+    count -= 1
+    }
+}
